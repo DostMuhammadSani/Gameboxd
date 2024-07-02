54c03e35524de76c7c7f7c133888c61d850d68cc
unknown <dostmuhammadsani@gmail.com>
2024-07-02 05:09:19 -0700

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './HomePage.css'; 
+import image from './Pics/1.jpg'; 
 
-function App() {
+const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home-page">
+      <img src={image} alt="Background" className="background-image" />
+      <nav className="navbar">
+        <div className="navbar-left">Gameboxd</div>
+        <div className="navbar-right">
+          <button>Sign In</button>
+          <button>Sign Up</button>
+        </div>
+      </nav>
+       <div className="center-content">
+        <h1>Welcome to Gameboxd</h1>
+        <button className="main-button">Get Started</button>
+       
+  
+      </div>
+      <div className='footer'>
+        <strong>
+        The social network for game lovers.
+        </strong>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default HomePage;
